5526ac31ff59753178f7f004c6bf022a432afc05
樺太庁長官 <122600023+PrefKarafuto@users.noreply.github.com>
2023-08-06 16:50:07 +0900

    Update system.pl

--- test/module/system.pl
+++ test/module/system.pl
@@ -246,7 +246,7 @@ sub InitSystemValue
 		'DATA'		=> '/datas',									# 初期データ設置パス
 		'BBSPATH'	=> '..',									# 掲示板設置パス
 		'DEBUG'		=> 0,										# デバグモード
-		'VERSION'	=> '0ch+ BBS dev-r103 20230705',						# CGIバージョン
+		'VERSION'	=> '0ch+ BBS dev-r104 20230806',						# CGIバージョン
 		'PM-DAT'	=> 0644,									# datパーミション
 		'PM-STOP'	=> 0444,									# スレストパーミション
 		'PM-TXT'	=> 0644,									# TXTパーミション
