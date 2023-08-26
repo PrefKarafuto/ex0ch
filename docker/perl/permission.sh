cc275dcd858548ef9717fc91daa937615c973a33
NaKa2355 <24926264+NaKa2355@users.noreply.github.com>
2023-08-26 15:26:34 +0900

    とりあえずPerl版をdockerで動かせるように dockerディレクトリ以下にREADMEを作成しました

---
+++ docker/perl/permission.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# パーミッションを設定するシェルスクリプト
+# http://sourceforge.jp/projects/zerochplus/wiki/Permissionを参考にしました
+
+#testディレクトリを置く場所
+ROOT_DIR=/usr/local/apache2/htdocs
+
+chmod 707 $ROOT_DIR/
+
+chmod 705 $ROOT_DIR/test/
+
+chmod 705 $ROOT_DIR/test/datas/
+chmod 604 $ROOT_DIR/test/datas/*
+chmod 600 $ROOT_DIR/test/datas/index.html
+
+chmod 707 $ROOT_DIR/test/info/
+chmod 707 $ROOT_DIR/test/info/.session/
+chmod 606 $ROOT_DIR/test/info/*cgi
+chmod 600 $ROOT_DIR/test/info/index.html
+
+chmod 705 $ROOT_DIR/test/module/
+chmod 604 $ROOT_DIR/test/module/*.pl
+chmod 600 $ROOT_DIR/test/module/index.html
+
+chmod 705 $ROOT_DIR/test/plugin/
+chmod 604 $ROOT_DIR/test/plugin/0ch_*.pl
+chmod 600 $ROOT_DIR/test/plugin/index.html
+
+chmod 707 $ROOT_DIR/test/plugin_conf/
+chmod 606 $ROOT_DIR/test/plugin_conf/0ch_*.cgi
+chmod 600 $ROOT_DIR/test/plugin_conf/index.html
+
+chmod 705 $ROOT_DIR/test/perllib/
+chmod 705 $ROOT_DIR/test/perllib/*/
+chmod 604 $ROOT_DIR/test/perllib/*.*
+chmod 600 $ROOT_DIR/test/perllib/index.html
+
+chmod 705 $ROOT_DIR/test/template/*.tt
+chmod 604 $ROOT_DIR/test/template/index.html
+
+chmod 705 $ROOT_DIR/test/*.cgi
+chmod 600 $ROOT_DIR/test/index.html
