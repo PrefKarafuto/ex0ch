957165c77a7e6131b3eca366ce7f7c2f0111983a
KarafutoAdmin <greqgs@goatmail.uk>
2023-11-25 22:43:51 +0900

    verup

--- test/module/system.pl
+++ test/module/system.pl
@@ -247,7 +247,7 @@ sub InitSystemValue
 		'BBSPATH'	=> '..',									# 掲示板設置パス
 		'SITENAME'	=> '',										# サイトの名前
 		'DEBUG'		=> 0,										# デバグモード
-		'VERSION'	=> 'ex0ch BBS dev-r126 20231115',			# CGIバージョン
+		'VERSION'	=> 'ex0ch BBS dev-r127 20231125',			# CGIバージョン
 		'PM-DAT'	=> 0644,									# datパーミション
 		'PM-STOP'	=> 0444,									# スレストパーミション
 		'PM-TXT'	=> 0644,									# TXTパーミション
