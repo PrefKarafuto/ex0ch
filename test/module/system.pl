35d8c1c7043202c551fa38a860ff3c1c729e72b3
PrefKarafuto <asandare@eripo.net>
2023-12-20 17:28:11 +0900

    アップデート

--- test/module/system.pl
+++ test/module/system.pl
@@ -255,7 +255,7 @@ sub InitSystemValue
 		'BBSPATH'	=> '..',									# 掲示板設置パス
 		'SITENAME'	=> '',										# サイトの名前
 		'DEBUG'		=> 0,										# デバグモード
-		'VERSION'	=> 'ex0ch BBS dev-r130 20231218',			# CGIバージョン
+		'VERSION'	=> 'ex0ch BBS dev-r131 20231220',			# CGIバージョン
 		'PM-DAT'	=> 0644,									# datパーミション
 		'PM-STOP'	=> 0444,									# スレストパーミション
 		'PM-TXT'	=> 0644,									# TXTパーミション
