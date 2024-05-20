c31134d84d789ca7d3f1ba0c3055be0f39f3e343
PrefKarafuto <asandare@eripo.net>
2024-05-20 19:28:18 +0900

    FastCGI表示

--- test/module/bbs_service.pl
+++ test/module/bbs_service.pl
@@ -591,11 +591,13 @@ HTML
 	$year += 1900;
 	my $lastMod = sprintf("Last modified : %d/%02d/%02d %02d:%02d:%02d",$year,$mon,$day,$hour,$min,$sec);
 	$Page->Print("<div align=\"center\" style=\"font-size: 0.8em; color: #933;\">$lastMod</div>");
+
+	my $is_fcgi = $ENV{'FCGI_ROLE'} ? '/FastCGI' : '';
 	
 	$Page->Print(<<FOOT);
 <div style="margin-top:1.2em;">
 <a href="https://github.com/PrefKarafuto/ex0ch">EXぜろちゃんねる</a>
-BBS.CGI - $ver (Perl)
+BBS.CGI - $ver (Perl$is_fcgi)
 @{[ $Sys->Get('DNSBL_TOREXIT') ? '+dan.me.uk' : '' ]}
 @{[ $Sys->Get('DNSBL_S5H') ? '+S5H' : '' ]}
 @{[ $Sys->Get('DNSBL_DRONEBL') ? '+DeoneBL' : '' ]}
